e57de669810aa0b5727e46835f02976d9656f5da
Andrew Clissold <andrewclissold@gmail.com>
2015-02-03 17:42:24 -0600

    Use symbolic constants in 1-5

--- 1-5.c
+++ 1-5.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+#define UPPER 300
+#define LOWER 0
+#define STEP  20
+
 /* print Fahrenheit-Celius table, albeit shorter than 1-3 */
 int main()
 {
     int fahr;
 
-    for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+    for (fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
 	  printf("%3d\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
     }
 
