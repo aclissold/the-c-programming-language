72c173767d31845a48a26f45b769df3a304f3d37
Andrew Clissold <andrewclissold@gmail.com>
2015-02-07 14:42:46 -0600

    Exercise 1-11

---
+++ 1-11.c
@@ -0,0 +1,4 @@
+/* The word count program could be tested by creating a collection of files
+ * with known line, word, and character counts and asserting that the program
+ * returns the expected results. Input that might catch bugs include corner
+ * cases, or input causing conditionals to not function as intended. */
